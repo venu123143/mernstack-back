435ab54b1a1dd2daca6c7456c3164ec3e7585c6c
venu reddy <venugopalreddy9493@gmail.com>
2023-10-22 09:12:57 +0530

    go live

--- dist/index.js
+++ dist/index.js
@@ -20,7 +20,7 @@ import ColorRouter from "./routes/ColorRoute.js";
 import CouponRouter from "./routes/CoponRoute.js";
 import EnquiryRouter from "./routes/EnqRoute.js";
 const options = {
-    origin: ['https://amazonadmin-app.netlify.app/', 'https://amazonadmin-app.netlify.app/', 'http://localhost:5173', 'http://localhost:5174'],
+    origin: ['https://amazonadmin-app.netlify.app/', 'https://main--amazonadmin-app.netlify.app/', 'https://amazon-clone-wtj7.onrender.com/', 'http://localhost:5173', 'http://localhost:5174'],
     credentials: true,
     withCredentials: true,
     optionSuccessStatus: 200,
